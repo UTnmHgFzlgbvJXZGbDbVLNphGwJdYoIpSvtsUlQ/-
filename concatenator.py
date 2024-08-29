993820b825255a8b78d66c27df97596240808d3e
UTnmHgFzlgbvJXZGbDbVLNphGwJdYoIpSvtsUlQ <166678901+UTnmHgFzlgbvJXZGbDbVLNphGwJdYoIpSvtsUlQ@users.noreply.github.com>
2024-08-29 23:08:00 +0800

    Update concatenator.py
    optimize algorithm

--- concatenator.py
+++ concatenator.py
@@ -33,12 +33,14 @@ def add():
 def delete():
  global c
  if len(d)!=8:
-  for x in range(4):
-   d.pop().destroy()
+  d.pop().destroy()
+  d.pop().destroy()
+  d.pop().destroy()
+  d.pop().destroy()
   c-=1
 def generate():
  try:
-  x,y,s,t=[],[],[],[]
+  x,y=[],[]
   for z in d[1::4]:
    z=Image.open(z.get())
    x.append(z)
@@ -54,44 +56,40 @@ def generate():
   for x,z in zip(range(1,len(y)),d[3::4]):
    y[x]*=.4
    w-=y[x-1]+y[x]
-   z=open(z.get(),'rb').read().replace(b'\r\n',b'\n').replace(b'\r',b'\n')
-   for v in range(128,256):
-    z=z.replace(bytes([v]),b'BYTE'+hex(v)[2:].encode())
-   z=etree.fromstring(z)
-   for v in z.findall('./Room'):
-    s.append(etree.tostring(v))
-   for v in z.findall('./Properties'):
-    for u in v:
-     t.append(etree.tostring(u))
-   for v in z.findall('.//AbsoluteLocation'):
+   z=etree.fromstring(eval(f"{open(z.get(),'rb').read().replace(b'\r\n',b'\n').replace(b'\r',b'\n')}"[1:]))
+   for v in z.xpath('./Properties'):
+    v[-1].tail='\n    '
+    try:
+     t.extend(v)
+    except UnboundLocalError:
+     t=v
+    z.remove(v)
+   for v in z.xpath('.//AbsoluteLocation'):
     u=v.get('value').split()
     v.set('value',f'{u[0]} {float(u[1])+w}')
-   for v in z.findall('.//Object'):
+   for v in z.xpath('.//Object'):
     v.set('name',f"{x}x{v.get('name')}")
-   for v in z.findall('.//Properties'):
-    r=v.findall('.//Property[@name="PathIsGlobal"][@value="1"]')
+   for v in z.xpath('.//Properties'):
+    r=v.xpath('.//Property[@name="PathIsGlobal"][@value="1"]')
     for u in v:
      q=u.get('name').strip('0123456789')
      if q in['ConnectedConverter','ConnectedObject',
-     'ConnectedSpout','Parent']and u.get('value')!='SWAMPY_SPOUT':
+      'ConnectedSpout','Parent']and u.get('value')!='SWAMPY_SPOUT':
       u.set('value',f"{x}x{u.get('value')}")
      elif r and q=='PathPos':
       q=u.get('value').split()
       u.set('value',f'{q[0]} {float(q[1])+w/.8}')
-   u=[]
-   for v in z:
-    if v.tag=='Object':
-     u.append(etree.tostring(v))
-   y[x-1]=b''.join(u)
-  y.insert(0,b'<Objects>\n  ')
-  y[-1]=s[-1]
-  y.append(b'  <Properties>\n    ')
-  y+=t
-  y.append(b'</Properties>\n</Objects>\n')
-  x=etree.tostring(etree.fromstring(b''.join(y)),pretty_print=1).split(b'BYTE')
-  for z in range(1,len(x)):
-   x[z]=bytes([int(x[z][:2],16)])+x[z][2:]
-  open(g.get(),'wb').write(b'<?xml version="1.0"?>\n'+b''.join(x))
+   for v in z.xpath('./Room'):
+    z.remove(v)
+   try:
+    s.extend(z)
+   except UnboundLocalError:
+    s=z
+  v.tail='\n  '
+  t[-1].tail='\n  '
+  s.append(v)
+  s.append(t)
+  open(g.get(),'wb').write(b'<?xml version="1.0"?>\n'+bytes(ord(x)for x in etree.tounicode(s)))
   e['fg']='green'
   e.delete('1.0','end')
   e.insert('1.0','Success!')
@@ -99,10 +97,14 @@ def generate():
   e['fg']='red'
   e.delete('1.0','end')
   e.insert('1.0',f'''{x.__class__.__name__}: {x} (Error code: {'8'.join([y.append(w)if w else
-[oct(z.tb_lineno)[2:]for z in y]for y in[[x.__traceback__]]for z in y for w in[z.tb_next]][-1])+')'}''')
+[oct(z.tb_lineno)[2:]for z in y]for y in[[x.__traceback__]]for z in y for w in[z.tb_next]][-1])})''')
 tk.Button(a,text='Generate',command=generate).pack(anchor='n',side='right',padx=5,pady=5)
 tk.Button(a,text='Delete level',command=delete).pack(anchor='n',side='right',pady=5)
 tk.Button(a,text='Add level',command=add).pack(anchor='n',side='right',padx=5,pady=5)
 add()
 add()
 a.mainloop()
+[0][0]='''
+room
+scene property
+'''
